b1105d143972152d49c7f75f9ffd3946a3333903
pamkaya <143752021+kasun777b@users.noreply.github.com>
2024-01-28 17:39:05 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', message="🎉 Alive 🎉 මැරුනේ නෑ පගෝ ✨ ✨ ")
+    return "🎉 Alive 🎉 මැරුනේ නෑ පගෝ ✨ ✨"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():
+def keep_alive():  
     t = Thread(target=run)
     t.start()
-
-# Call keep_alive() to start the Flask app in the background
-keep_alive()
